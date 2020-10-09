d158d67c7084778b3d74776f85ac0474254854cd
rapixar <cudraphael@gmail.com>
2020-10-09 14:14:43 +0100

    Password validation editing the password for the database and migrating the schema for the apps therein via the CLI

--- Backend/django_app/mainapp/local_settings.py
+++ Backend/django_app/mainapp/local_settings.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'floweapp',
         'USER': 'floweapp',
-        'PASSWORD': 'Christian97',
+        'PASSWORD': 'floweapp',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
