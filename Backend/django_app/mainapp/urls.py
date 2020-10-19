8bd24cfa4a829aa12fa106ef5a093c6c540340be
rapixar <cudraphael@gmail.com>
2020-10-19 10:16:42 +0100

    Added authentication packages to INSTALLED_APPS

--- Backend/django_app/mainapp/urls.py
+++ Backend/django_app/mainapp/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
-i"""mainapp URL Configuration
+"""mainapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
